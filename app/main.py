c69e04299c5d7d0cc7ba1f6248b61b0dca441c44
Aziz Sebri <sebriaziz2016@gmail.com>
2025-03-24 14:24:44 +0100

    improving interface and add a history for the messages

--- app/main.py
+++ app/main.py
@@ -1,42 +1,56 @@
 import json
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from typing import List
+from typing import List, Dict
 
 app = FastAPI()
 
-# Dictionnaire pour garder la connexion WebSocket de chaque utilisateur
-active_connections: dict = {}
 
-# Liste d'utilisateurs simulée (vous pouvez la remplacer par une base de données ou une autre source dynamique)
-users = ["user1", "user2", "user3"]
+active_connections: Dict = {}
+
+messages_history: Dict[str, List[Dict[str, str]]] = {}
 
 @app.websocket("/ws/{username}")
 async def websocket_endpoint(websocket: WebSocket, username: str):
-    # Accepter la connexion WebSocket
     await websocket.accept()
-    active_connections[username] = websocket  # Enregistrer la connexion WebSocket du client
+    print(f"{username} is connected now!")
+    active_connections[username] = websocket
+
+    await websocket.send_json({
+        "type": "user_list",
+        "users": [user for user in active_connections.keys() if user != username]
+    })
+
+    
+    if username in messages_history:
+        for msg in messages_history[username]:
+            await websocket.send_json(msg)
 
     try:
-        # Envoyer la liste des utilisateurs au client dès la connexion
-        await websocket.send_json({"type": "user_list", "users":list(active_connections.keys())})
-
         while True:
-            # Attendre et recevoir des messages
+            #wait the message from the client 
             data = await websocket.receive_text()
             message_data = json.loads(data)
             receiver = message_data.get("receiver")
             message = message_data.get("message")
 
-            # Envoyer le message au destinataire s'il est connecté
+            #save the message in the history to retrieve it later
+            if receiver not in messages_history:
+                messages_history[receiver] = []
+            messages_history[receiver].append({
+                "sender": username,
+                "message": message
+            })
+
+            #send the message to the receiver if he is connected
             if receiver in active_connections:
                 await active_connections[receiver].send_text(json.dumps({
                     "sender": username,
                     "message": message
                 }))
             else:
-                print(f"Utilisateur {receiver} n'est pas connecté.")
+                print(f"L'utilisateur {receiver} n'est pas connecté.")
                 
     except WebSocketDisconnect:
-        # Supprimer la connexion WebSocket lorsque l'utilisateur se déconnecte
+        # delete the user from the active connections if he disconnect
         active_connections.pop(username, None)
         print(f"{username} a quitté la conversation.")
